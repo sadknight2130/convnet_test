91df1d4f2a38beb9d24a0e959702e41bf6bdb60e
sadknight2130 <yintianping95@163.com>
2020-11-21 12:44:07 +0800

    add annotation

--- finetuning_test.py
+++ finetuning_test.py
@@ -23,31 +23,36 @@ feature_extract = True
 input_size = 224
 mode_dict = ["train", "val"]
 
+# Load数据
 dataloaders_dict = load_data(data_dir, input_size, batch_size, mode_dict)
+
+# 加载模型
 model_ft, input_size = initialize_model(model_name,
                                         num_classes, feature_extract, use_pretrained=True)
-
-
 model_ft = model_ft.to(device)
+# 定义优化器和loss函数
 optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad,
-                                   model_ft.parameters()), lr=0.001, momentum=0.9)  #只更新requires_grad = True的参数
+                                   model_ft.parameters()), lr=0.001, momentum=0.9)  # 只更新requires_grad = True的参数
 loss_fn = nn.CrossEntropyLoss()
+# 训练模型 返回最终模型和val上的准确率数据
 _, ohist = train_model(model_ft, dataloaders_dict, loss_fn, optimizer, num_epochs=num_epochs)
 
+# 加载 和 训练 fine tuning模型
 model_scratch, _ = initialize_model(model_name,
-                    num_classes, feature_extract=False, use_pretrained=False)
+                                    num_classes, feature_extract=False, use_pretrained=False)
 model_scratch = model_scratch.to(device)
 optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad,
                                    model_scratch.parameters()), lr=0.001, momentum=0.9)
 loss_fn = nn.CrossEntropyLoss()
 _, scratch_hist = train_model(model_scratch, dataloaders_dict, loss_fn, optimizer, num_epochs=num_epochs)
 
+# 画出feature extract 和 fine tuning 模型在val上的准确率曲线
 plt.title("Validation Accuracy vs. Number of Training Epochs")
 plt.xlabel("Training Epochs")
 plt.ylabel("Validation Accuracy")
-plt.plot(range(1,num_epochs+1),ohist,label="Pretrained")
-plt.plot(range(1,num_epochs+1),scratch_hist,label="Scratch")
-plt.ylim((0,1.))
-plt.xticks(np.arange(1, num_epochs+1, 1.0))
+plt.plot(range(1, num_epochs + 1), ohist, label="Pretrained")
+plt.plot(range(1, num_epochs + 1), scratch_hist, label="Scratch")
+plt.ylim((0, 1.))
+plt.xticks(np.arange(1, num_epochs + 1, 1.0))
 plt.legend()
 plt.show()
